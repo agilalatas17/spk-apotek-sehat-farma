c954dd53971f95d107194e86e2720686a7cb8ff0
Adhim <adhimm234@gmail.com>
2025-07-22 02:15:53 +0700

    menambahkan protected fillable

--- app/Models/NilaiAlternatif.php
+++ app/Models/NilaiAlternatif.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class NilaiAlternatif extends Model
 {
+    use HasFactory;
+
+    protected $fillable = [
+        'obat_id',
+        'kriteria_id',
+        'nilai',
+        'crip_id'
+    ];
+
     public function obat()
     {
         return $this->belongsTo(Obat::class);
@@ -16,7 +25,7 @@ class NilaiAlternatif extends Model
     {
         return $this->belongsTo(Kriteria::class);
     }
-    
+
     public function crip()
     {
         return $this->belongsTo(Crip::class);
